e871dea781a21560c3ef754f3edadebb72532b02
Mehmet Anıl ÜLKÜ <85405127+Adonis-Lee@users.noreply.github.com>
2026-04-15 14:56:58 +0300

    Beginning

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+    
+    public float speed;
+    
+    void Start()
+    {
+        rb  = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+}
